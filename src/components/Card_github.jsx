8bf4e3d85be0ae111c22614d58a0ca8085f4eac9
Rakesh Ranjan Ray <82263592+Rakesh8969Engineer@users.noreply.github.com>
2023-09-08 01:19:56 +0530

    Update Card_github.jsx

--- src/components/Card_github.jsx
+++ src/components/Card_github.jsx
@@ -1,30 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import '../Card_github.css'
+import "../Card_github.css"
 
-const Card_github = ({ username }) => {
-  const [userInfo, setUserInfo] = useState(null);
+const GitHubInfoCard = ({ username }) => {
+  const [userData, setUserData] = useState(null);
 
   useEffect(() => {
-
+  
     fetch(`https://api.github.com/users/${username}`)
       .then((response) => response.json())
-      .then((data) => setUserInfo(data))
+      .then((data) => setUserData(data))
       .catch((error) => console.error(error));
   }, [username]);
 
-  if (!userInfo) {
-    return <div>Gettind Data...</div>;
+  if (!userData) {
+    return <div>Loading...</div>;
   }
+
   return (
     <div className="Card">
-      <img src={userInfo.avatar_url} alt="User Avatar" />
-      <h2>{userInfo.login}</h2>
-      <p>Name: {userInfo.name}</p>
-      <p>Public Repos: {userInfo.public_repos}</p>
-      <p>Public Gists: {userInfo.public_gists}</p>
-      <p>Profile Created At: {new Date(userInfo.created_at).toLocaleDateString()}</p>
+      <img src={userData.avatar_url} alt="User Avatar" />
+      <h2>{userData.login}</h2>
+      <p>Name: {userData.name}</p>
+      <p>Public Repos: {userData.public_repos}</p>
+      <p>Public Gists: {userData.public_gists}</p>
+      <p>Profile Created At: {new Date(userData.created_at).toLocaleDateString()}</p>
     </div>
   );
 };
 
-export default Card_github;
+export default GitHubInfoCard;
